75ab15532dc112d21e51b0b46e1bf1f7f7604c20
Oliviasamey <123127436+Oliviasamey@users.noreply.github.com>
2024-02-29 11:31:30 +0000

    Delete Angular directory

--- Angular/inmest-web - Copy/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './main-features/dashboard/dashboard.component';
-import { UsersComponent } from './main-features/users/users.component';
-
-export const routes: Routes = [
-    {path:'', component: DashboardComponent},
-    {path:'users', component: UsersComponent}
-];
